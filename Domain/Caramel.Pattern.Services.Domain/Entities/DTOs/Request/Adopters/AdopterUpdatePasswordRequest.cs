a779a582e741eb4f663c63a18db807a5ea7253a9
André Cini <andrevpcini@gmail.com>
2024-11-20 17:28:15 -0300

    [FEAT] - Criação de rota para redefinir a senha

--- Domain/Caramel.Pattern.Services.Domain/Entities/DTOs/Request/Adopters/AdopterUpdatePasswordRequest.cs
+++ Domain/Caramel.Pattern.Services.Domain/Entities/DTOs/Request/Adopters/AdopterUpdatePasswordRequest.cs
@@ -5,7 +5,6 @@ namespace Caramel.Pattern.Services.Domain.Entities.DTOs.Request.Adopters
     [ExcludeFromCodeCoverage]
     public class AdopterUpdatePasswordRequest
     {
-        public string Id { get; set; }
         public string Email { get; set; }
         public string NewPassword { get; set; }
     }
